083607dc7e55ce000c4f6727e111dd8f56e5442a
Careertutorialknjsd <careertutorialknj@gmail.com>
2026-07-15 09:44:27 +0530

    INITIAL CHANGE

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://payment-portal-virid-two.vercel.app/api',
 });
 
 api.interceptors.request.use((config) => {
